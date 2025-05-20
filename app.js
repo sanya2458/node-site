6c05c3fd6c8edf62c80c9e164c128db8e527ca37
sanya2458 <156212096+sanya2458@users.noreply.github.com>
2025-05-20 13:07:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -94,5 +94,7 @@ app.post('/delete/:id', isAdmin, (req, res) => {
   });
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на порту ${PORT}`);
 });
