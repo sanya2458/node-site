62a77b5a0493d2d0ca816c25ccc3f0f4504f0bd5
sanya2458 <156212096+sanya2458@users.noreply.github.com>
2025-05-21 22:34:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -169,12 +169,6 @@ app.get('/', (req, res) => {
     html += `<a href="/login" class="button-link">Увійти</a>`;
   }
 
-  html += `
-          </div>
-        </div>
-        <div class="container">
-          <h1>Пости</h1>
-  `;
 
   if (posts.length === 0) {
     html += `<p>Постів поки що немає.</p>`;
