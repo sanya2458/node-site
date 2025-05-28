a0bf0474840ec3e570185daea31606ab3ac3f9ed
sanya2458 <156212096+sanya2458@users.noreply.github.com>
2025-05-28 21:20:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
